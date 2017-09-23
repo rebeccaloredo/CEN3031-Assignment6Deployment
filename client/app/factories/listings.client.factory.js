0d9f6887dc5a9a0c2dadfac300b0b36f10882316
rebeccaloredo <rebeccaloredo@users.noreply.github.com>
2017-09-22 23:06:05 -0400

    figuring it out

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -2,23 +2,23 @@ angular.module('listings').factory('Listings', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('quiet-wave-69460.herokuapp.com/api/listings');
+        return $http.get('/api/listings');
       },
 
       create: function(listing) {
-        return $http.post('quiet-wave-69460.herokuapp.com/api/listings', listing);
+        return $http.post('/api/listings', listing);
       },
 
       read: function(id) {
-        return $http.get('quiet-wave-69460.herokuapp.com/api/listings' + id);
+        return $http.get('/api/listings' + id);
       },
 
       update: function(id, listing) {
-        return $http.put('quiet-wave-69460.herokuapp.com/api/listings' + id, listing);
+        return $http.put('/api/listings' + id, listing);
       },
 
       delete: function(id) {
-        return $http.delete('quiet-wave-69460.herokuapp.com/api/listings' + id);
+        return $http.delete('/api/listings' + id);
       }
     };
 
